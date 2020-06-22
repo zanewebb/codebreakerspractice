e9564e11c816f3734f29c6cfda3315f865796518
zanewebb <zane.webb97@gmail.com>
2020-06-22 14:32:28 -0700

    more practice

--- Sorting/Merge.py
+++ Sorting/Merge.py
@@ -2,6 +2,23 @@
 
 class Solution:
 
+    # 4th time
+
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        if len(intervals) <= 1:
+            return intervals
+        
+        intervals = sorted(intervals, key= lambda x: x[0])
+        i = 1
+        while i < len(intervals):
+            if intervals[i][0] <= intervals[i-1][1]:
+                intervals[i-1][0] = min(intervals[i-1][0],intervals[i][0])
+                intervals[i-1][1] = max(intervals[i-1][1],intervals[i][1])
+                intervals.pop(i)
+                i -= 1
+            i += 1
+        return intervals
+
     # 3rd time??
     # Using the "place" var helped a lot in my thinking 
 
